81447fde9443e98209c31bda31c82e9bf738d17c
braannn <syabran99@gmail.com>
2020-05-20 10:42:15 +0700

    add level screen

---
+++ HiFit/Beginner.swift
@@ -0,0 +1,32 @@
+//
+//  Beginner.swift
+//  HiFit
+//
+//  Created by Muhammad Syabran on 20/05/20.
+//  Copyright © 2020 AbdulRazak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Beginner : UIViewController {
+    
+    @IBOutlet weak var TitleBegin: UILabel!
+    
+    var titleLabel: String = "Beginner"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        TitleBegin?.text = "\(titleLabel)"
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let viewController = segue.destination as! LevelScreen
+        viewController.beginTitle = self.TitleBegin.text!
+    }
+    
+    
+}
--- HiFit/LevelScreen.swift
+++ HiFit/LevelScreen.swift
@@ -15,9 +15,15 @@ class LevelScreen : UIViewController {
     @IBOutlet weak var levelBeginner: UIButton!
     @IBOutlet weak var levelIntermediate: UIButton!
     @IBOutlet weak var levelAdvanced: UIButton!
+    @IBOutlet weak var BeginLabel: UILabel!
+    
+    var beginTitle: String = ""
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        level()
         
         let buttonSelectorRadius:CGFloat = 20
         
@@ -25,15 +31,19 @@ class LevelScreen : UIViewController {
         levelIntermediate.layer.cornerRadius = buttonSelectorRadius
         levelAdvanced.layer.cornerRadius = buttonSelectorRadius
         
-        
         // Button Normal State
         levelBeginner.setBackgroundImage(#imageLiteral(resourceName: "BeginnerBg"), for: .normal)
         levelIntermediate.setBackgroundImage(#imageLiteral(resourceName: "IntermediateBg"), for: .normal)
         levelAdvanced.setBackgroundImage(#imageLiteral(resourceName: "AdvancedBg"), for: .normal)
         
-        
         levelBeginner.clipsToBounds = true
         levelIntermediate.clipsToBounds = true
         levelAdvanced.clipsToBounds = true
+        
+        BeginLabel?.text = "\(beginTitle)"
+    }
+    func level() {
+        let Level = defaults.value(forKey: self.beginTitle) as? String ?? ""
+        BeginLabel.text = Level
     }
 }
