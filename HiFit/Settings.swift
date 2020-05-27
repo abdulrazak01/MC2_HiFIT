991210578d3e671fd1bfe41f5601e8e869bad9bf
Omar Tan <omartan90@hotmail.com>
2020-05-28 03:36:48 +0800

    Tidied up Gradient extension

--- HiFit/Settings.swift
+++ HiFit/Settings.swift
@@ -51,16 +51,25 @@ class Settings : UIViewController {
 
 extension UIButton
 {
-    func applyGradient(colors: [CGColor], radius: CGFloat = 0)
+    /**
+    # Apply Gradient
+     UIButton Extension to make applying gradient a breeze
+     
+     Check out [AppCoda](https://www.appcoda.com/cagradientlayer/) for gradient direction info
+     
+     - Parameters:
+       - colors: Minimum 2 colors
+       - radius: Optional
+       - startGradient: Optional, range: 0.0 - 1.0
+       - endGradient: Optional, range: 0.0 - 1.0
+    */
+    func applyGradient(colors: [CGColor], radius:CGFloat = 0, startGradient:CGPoint = CGPoint(x: 0.5, y: 0.0), endGradient:CGPoint = CGPoint(x: 0.5, y: 1.0))
     {
         let gradientLayer = CAGradientLayer()
         gradientLayer.cornerRadius = radius
         gradientLayer.colors = colors
-        
-        // Configure gradient direction https://www.appcoda.com/cagradientlayer/
-        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
-        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
-        
+        gradientLayer.startPoint = startGradient
+        gradientLayer.endPoint = endGradient
         gradientLayer.frame = self.bounds
         self.layer.insertSublayer(gradientLayer, at: 0)
     }
