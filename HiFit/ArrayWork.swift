343d14cffc9e692707942564077968cff3b8d9ef
Omar Tan <omartan90@hotmail.com>
2020-05-30 11:59:38 +0800

    Tidied up array workout Replaced exercises with proper demo exercises

--- HiFit/ArrayWork.swift
+++ HiFit/ArrayWork.swift
@@ -19,50 +19,54 @@ class ArrayWork{
     var list = [workout]()
     
     init(){
+        // MARK: - Neck Mobility
         list.append(workout(image: "pushup", WorkText: "Neck Mobility", promptText:
             "1. Tuck chin into chest, lift chin upward as far as possible 2. Repeat for 10 seconds 3. Lower left ear towards left shoulder and then the other side. 4. Repeat for 10 seconds 5. Turn your chin laterally towards left shoulder and rotate towards right shoulder 6. Repeat till end of duration", typeText: "Warm Up", timer: "\(warmUpDuration)", goTimer: warmUpDuration, Numb: 1))
         
-        list.append(workout(image: "jump", WorkText: "Arm Swings", promptText:
-            "1. Feet slightly wider than shoulder-width apart 2. Knees slightly bent 3. Swing both arms continuously to an overhead position 4. Then forward, down and back 5. Repeat for 15 seconds 6. Swing both arms to sides then cross them in front of chest 7. Repeat for 15 seconds", typeText: "Exercise", timer: "\(exerciseDuration)", goTimer: exerciseDuration, Numb: 0))
+        // MARK: - Jumping Jacks
+        list.append(workout(image: "jump", WorkText: "Jumping Jacks", promptText:
+            "1. Stand with legs straight and arm by your sides 2. Jump up and spread feet beyond hip-width apart 3. Bring arms above head 4. Jump again, lower arm, bring legs together 5. Return to starting position", typeText: "Exercise", timer: "\(exerciseDuration)", goTimer: exerciseDuration, Numb: 0))
         list.append(workout(image: "situp", WorkText: "Rest", promptText:
             "", typeText: "Rest", timer: "\(restDuration)", goTimer: restDuration, Numb: 0))
         
-        list.append(workout(image: "situp", WorkText: "Side Bends", promptText:
-            "1. Feet slightly wider than shoulder-width apart 2. Knees slightly bent, hands resting on hips 3. Bend to one side, avoid leaning front or back 4. Return to upright position 5. Repeat other side 6. Maintain a slow rhythm for 30 seconds", typeText: "Exercise", timer: "\(coolDownDuration)", goTimer: coolDownDuration, Numb: 1))
+        // MARK: - Push Up
+        list.append(workout(image: "situp", WorkText: "Push Up", promptText:
+            "1. Hands on the floor 2. Shoulder width apart 3. Engage your core 4. Exhale when lowering your chest 5. Inhale when push back up 6. Maintain flat back", typeText: "Exercise", timer: "\(coolDownDuration)", goTimer: coolDownDuration, Numb: 1))
         list.append(workout(image: "situp", WorkText: "Rest", promptText:
             "", typeText: "Rest", timer: "\(restDuration)", goTimer: restDuration, Numb: 1))
         
-        list.append(workout(image: "situp", WorkText: "Hip Circles", promptText:
-            "1. Hands on hips, feet wider than shoulders 2. Rotate your hips in clockwise direction for 15 seconds 3. Rotate your hips in the other direction for 15 seconds", typeText: "Exercise", timer: "\(exerciseDuration)", goTimer:exerciseDuration, Numb: 2))
-        
+        // MARK: - Plank
+        list.append(workout(image: "situp", WorkText: "Plank", promptText:
+            "1. Get into a push up position 2. Keep palms and toes on ground 3. Keep back straight, core muscles tight 4. Don’t let your head or back sag towards the floors 5. Hold plank throughout the duration", typeText: "Exercise", timer: "\(exerciseDuration)", goTimer:exerciseDuration, Numb: 2))
         list.append(workout(image: "situp", WorkText: "Rest", promptText:
             "", typeText: "Rest", timer: "\(restDuration)", goTimer: restDuration, Numb: 2))
         
-        list.append(workout(image: "situp", WorkText: "Hip Twists", promptText:
-            "1. Extend arms to sides 2. Twist torso and hips to left, shift weight to left foot 3. Turn body to right, shift weight to right foot 4. Repeat for 30 seconds", typeText: "Exercise", timer: "\(exerciseDuration)", goTimer: exerciseDuration, Numb: 3))
-        
+        // MARK: - Air Swimming
+        list.append(workout(image: "situp", WorkText: "Air Swimming", promptText:
+            "1. Lie on stomach with arms extended 2. Lift chest, arms and legs 3. Squeeze your glutes 4. Flutter arms and legs while not touching the floor", typeText: "Exercise", timer: "\(exerciseDuration)", goTimer: exerciseDuration, Numb: 3))
         list.append(workout(image: "situp", WorkText: "Rest", promptText:
             "", typeText: "Rest", timer: "\(restDuration)", goTimer: restDuration, Numb: 3))
         
-        list.append(workout(image: "situp", WorkText: "Half Squat", promptText:
-            "1. Hold hands in front of your for balance 2. Bend knees till thighs 45 degrees from the floor 3. Keep back straight and look to the front 4. Knees pointed same direction with toes 5. At lowest point, straighten legs to return to starting position 6. Repeat in a smooth and controlled rhythm 7. Breath in as you descent, breath out as your rise 8. Repeat for the whole duration", typeText: "Exercise", timer: "\(exerciseDuration)", goTimer: exerciseDuration, Numb: 4))
-        
+        // MARK: - Jump Squat
+        list.append(workout(image: "situp", WorkText: "Jump Squat", promptText:
+            "1. Stand with feet shoulder-width apart 2. Toes slightly turned out, hands in front of chest 3. When lowering, engage core, chest lifted and flat back 4. Maintain posture and jump as high as you can 5. Swinging arms down by your sides for momentum 6. Land softly, avoid knees from moving inwards", typeText: "Exercise", timer: "\(exerciseDuration)", goTimer: exerciseDuration, Numb: 4))
         list.append(workout(image: "situp", WorkText: "Rest", promptText:
             "", typeText: "Rest", timer: "\(restDuration)", goTimer: restDuration, Numb: 4))
         
-        list.append(workout(image: "situp", WorkText: "Reverse Lunge with Left Leg", promptText:
-            "1. Stand with feet shoulder-width apart 2. Step your left foot behind bending your knees 3. Push through the heel of left foot to stand up 4. Repea", typeText: "Exercise", timer: "\(exerciseDuration)", goTimer: exerciseDuration, Numb: 5))
-        
+        // MARK: - Half Push Up
+        list.append(workout(image: "situp", WorkText: "Half Push Up", promptText:
+            "1. Hands on the floor 2. Shoulder width apart 3. Knees on floor 4. Exhale when lowering your chest 5. Inhale when push back up", typeText: "Exercise", timer: "\(exerciseDuration)", goTimer: exerciseDuration, Numb: 5))
         list.append(workout(image: "situp", WorkText: "Rest", promptText:
-            "1. Rest for 30 seconds 2. Relax position 3. Enjoy Your Rest", typeText: "Rest", timer: "\(restDuration)", goTimer: restDuration, Numb: 5))
+            "", typeText: "Rest", timer: "\(restDuration)", goTimer: restDuration, Numb: 5))
         
-        list.append(workout(image: "situp", WorkText: "Reverse Lunge with Right Leg", promptText:
-            "1. Stand with feet shoulder-width apart 2. Step your right foot behind bending your knees 3. Push through the heel of right foot to stand up 4. Repeat", typeText: "Exercise", timer: "\(exerciseDuration)", goTimer: exerciseDuration, Numb: 6))
-        
+        // MARK: - Pike Push Up
+        list.append(workout(image: "situp", WorkText: "Pike Push Up", promptText:
+            "1. Get into a push up position 2. Form a triangle by raising your glutes and moving feet inwards 3. Bend elbows and lower your shoulder to the ground 4. Allow forehead to very lightly contact the ground 5. Push upwards and back to starting position", typeText: "Exercise", timer: "\(exerciseDuration)", goTimer: exerciseDuration, Numb: 6))
         list.append(workout(image: "situp", WorkText: "Rest", promptText:
             "", typeText: "Rest", timer: "\(restDuration)", goTimer: restDuration, Numb: 6))
         
-        list.append(workout(image: "situp", WorkText: "Shoulder Circles", promptText:
-            "", typeText: "Cool Down", timer: "\(coolDownDuration)", goTimer: coolDownDuration, Numb: 1))
+        // MARK: - Shoulder Stretch
+        list.append(workout(image: "situp", WorkText: "Shoulder Stretch", promptText:
+            "1. Feet slightly wider than shoulder width apart 2. Knees slightly bent 3. Place right arm parallel to the ground across your chest 4. Bend left arm up and use forearm to ease right arm 5. Closer to chest", typeText: "Cool Down", timer: "\(coolDownDuration)", goTimer: coolDownDuration, Numb: 1))
     }
 }
