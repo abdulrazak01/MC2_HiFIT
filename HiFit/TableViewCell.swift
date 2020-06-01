42310dcb5898955930d9a2af2d41ae7e94eede37
braannn <syabran99@gmail.com>
2020-06-01 17:42:09 +0700

    adding table view and title cell

---
+++ HiFit/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  HiFit
+//
+//  Created by Muhammad Syabran on 01/06/20.
+//  Copyright © 2020 AbdulRazak. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
