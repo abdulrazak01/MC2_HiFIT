c71e8d32c8e717a03e3e44ce48ea497f043b97e8
Rusmaldi <RianRusmaldi@gmail.com>
2020-06-13 14:32:58 +0700

    Speech Service

---
+++ HiFit/SpeechService.swift
@@ -0,0 +1,34 @@
+//
+//  SpeechService.swift
+//  HiFit
+//
+//  Created by Rian Rusmaldi on 12/06/20.
+//  Copyright © 2020 AbdulRazak. All rights reserved.
+//
+
+import AVFoundation
+import UIKit
+
+class SpeechService {
+    
+    let speechSynthesizer = AVSpeechSynthesizer()
+    
+    
+    func say(_ phrase:String){
+        
+//        guard UIAccessibility.isVoiceOverRunning else {
+//            return
+//        }
+        
+        let utterenece = AVSpeechUtterance(string: phrase)
+        let langCode = "en-EN".localized
+        utterenece.voice = AVSpeechSynthesisVoice(language: langCode)
+        speechSynthesizer.speak(utterenece)
+    }
+}
+
+extension String{
+    var localized : String{
+        return NSLocalizedString(self, comment: "")
+    }
+}
