e12349fcfc8cfdb94aedb3e782910631f4f52da8
Omar Tan <omartan90@hotmail.com>
2020-06-17 12:00:09 +0800

    UI Label Extension

---
+++ HiFit/UILabelExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  UILabelExtensions.swift
+//  HiFit
+//
+//  Created by Omar Tan Johan Tan on 17/06/20.
+//  Copyright © 2020 AbdulRazak. All rights reserved.
+//
+import UIKit
+
+extension UILabel {
+    func dynamicFont(textStyles: UIFont, weight: UIFont.Weight, dynamicType: Bool ) {
+        let style = textStyles.with(weight: weight)
+        self.font = UIFontMetrics.default.scaledFont(for: style)
+        self.adjustsFontForContentSizeCategory = dynamicType
+    }
+}
